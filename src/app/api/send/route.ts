1c6937488c9480ad5b483878fbee9e68a1517f45
v-Sharan <vsharansago@gmail.com>
2024-12-30 18:02:27 +0530

    contact form using react-hook-form

---
+++ src/app/api/send/route.ts
@@ -0,0 +1,26 @@
+import { Email } from "@/components";
+import { Resend } from "resend";
+
+const resend = new Resend(process.env.NEXT_PUBLIC_RESEND_API_KEY!);
+
+export async function POST(request: Request) {
+  const { email, subject, message } = await request.json();
+  console.log(email, subject, message);
+
+  try {
+    const { data, error } = await resend.emails.send({
+      from: "Info <info@resend.dev>",
+      to: ["vsharansago@gmail.com"],
+      subject: `Message From AMD-DRONES website and subject is ${subject}`,
+      react: Email({ email, subject, message }) as React.ReactElement,
+    });
+
+    if (error) {
+      return Response.json({ error }, { status: 500 });
+    }
+
+    return Response.json(data);
+  } catch (error) {
+    return Response.json({ error }, { status: 500 });
+  }
+}
